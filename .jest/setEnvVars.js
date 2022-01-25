3260e9c38d223718524555fea4f49e9e6d6e2d30
Remigiusz Orlowski <remigiusz.orlowski@gmail.com>
2022-01-25 17:04:49 +0100

    Get rid of access token provisioning

--- .jest/setEnvVars.js
+++ .jest/setEnvVars.js
@@ -3,4 +3,3 @@ process.env.CLOUDMAPS_SERVICE_HOST     = 'localhost:8151';
 process.env.AUTHORIZATION_EMAIL        = 'remi@kentik.com';
 process.env.AUTHORIZATION_API_TOKEN    = 'cafebabe';
 process.env.AWS_REGION                 = 'eu-central-1';
-process.env.AUTHORIZATION_ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJ1aWQiOiJyZW1pQGtlbnRpay5jb20iLCJ1c2VyX2lkIjo3MjU5LCJjb21wYW55X2lkIjoxMDEzLCJzY29wZXMiOiJhZG1pbi5jbG91ZF9tYXBzIGFkbWluLmNsb3VkX21hcHM6d3JpdGUifQ.CjLJLRT3CG0v-8oPiOjnqNteZunka22Xd4vLulXiMi0';
